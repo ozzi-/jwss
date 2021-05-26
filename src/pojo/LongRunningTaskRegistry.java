c657eab17e1f583ee515c3c95c497aa67f45048b
ozzi- <oz@oz-web.com>
2021-05-26 12:30:35 +0200

    scheduler cleanup

--- src/pojo/LongRunningTaskRegistry.java
+++ src/pojo/LongRunningTaskRegistry.java
@@ -1,8 +1,15 @@
 package service;
 
+import java.util.Date;
+import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
+import helpers.Log;
 import pojo.LongRunningTaskResult;
+
 
 public class LongRunningTaskRegistry {
     static ConcurrentHashMap<String, LongRunningTaskResult> longRunningTaskRegistryMap = new ConcurrentHashMap<>();
@@ -27,5 +34,17 @@ public class LongRunningTaskRegistry {
     	longRunningTaskRegistryMap.put(longRunningTaskID, lrtr);
     }
     
-    // TODO interval to automatically purge stale results
+	public static void scheduleCleanup()  {
+		Runnable task = () -> {
+			for (Entry<String, LongRunningTaskResult> res : longRunningTaskRegistryMap.entrySet()) {
+				Date date = new Date();
+				long timeMilli = date.getTime();
+			    if(res.getValue().getRegisteredTimestamp()+10000<timeMilli) {
+			    	longRunningTaskRegistryMap.remove(res.getKey());
+			    	Log.logInfo("Cleaned up long running task "+res.getKey()+" result as it was never requested", LongRunningTaskRegistry.class);
+			    }
+			}
+		};
+		scheduler.scheduleWithFixedDelay(task, 0, 30, TimeUnit.SECONDS);
+	}
 }
